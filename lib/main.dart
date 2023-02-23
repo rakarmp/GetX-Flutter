6aadfd913deffb15e0b43864144d31a9725b0404
grizz12 <raka_088@smkassalaambandung.sch.id>
2023-02-22 20:32:19 -0800

    change main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+// void main() {
+//   runApp(
+//     GetMaterialApp(
+//       title: "Application",
+//       initialRoute: AppPages.INITIAL,
+//       getPages: AppPages.routes,
+//     ),
+//   );
+// }
+
+void main() async {
+  await GetStorage.init();
   runApp(
     GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
